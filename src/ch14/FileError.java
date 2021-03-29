48692e6aeb5a1b3ece8eaaff6676b551f050d938
chlwltjd98 <chlwltjd98@naver.com>
2021-03-29 14:05:49 +0900

    test file [오류, 예외 처리]

---
+++ src/ch14/FileError.java
@@ -0,0 +1,29 @@
+package ch14;
+
+import java.io.*;
+
+public class FileError {
+	public static void main(String[] args) {
+		writeList();
+		
+	}
+	private static void writeList()  {
+		PrintWriter out = null;
+		FileWriter fw = null;
+		try {
+//			fw = new FileWriter("C:/abc/out.txt");
+			fw = new FileWriter("out2.txt");
+			out = new PrintWriter(fw);
+			out.println("Hello?");
+			
+		} catch (IOException e) {
+			System.out.println("catch : " +e.getMessage());
+		} finally {
+			System.out.println("finally code...");
+			if(out != null)
+			out.close();
+			System.out.println("finally end...");
+		}	
+		
+	}
+}
